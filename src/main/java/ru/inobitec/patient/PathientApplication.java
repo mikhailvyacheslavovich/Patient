6c39b88af9bc9396b764895dc075a745145709a2
Mike <mikhailvjacheslavovich@gmail.com>
2022-07-16 23:05:33 +0300

    Create spring project for MicroService Patient

---
+++ src/main/java/ru/inobitec/patient/PathientApplication.java
@@ -0,0 +1,13 @@
+package ru.inobitec.patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/inobitec/patient/PathientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.inobitec.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
