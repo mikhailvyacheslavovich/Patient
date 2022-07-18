4410296935eb8b3eea08781f1035128790a297c8
Mike <mikhailvjacheslavovich@gmail.com>
2022-07-18 19:34:12 +0300

    Some fixes

--- src/main/java/ru/inobitec/patient/model/Patient.java
+++ src/main/java/ru/inobitec/patient/model/Patient.java
@@ -1,14 +1,10 @@
 package ru.inobitec.patient.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Date;
 
-@Setter
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Patient {
